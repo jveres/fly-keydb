17bdd4ccb11f9b96f66f4f37fa7ea034423a6efd
jveres <janos.veres@gmail.com>
2022-03-25 14:35:37 +0100

    remove masters during graceful shutdown

--- utils/detect_peers.sh
+++ utils/detect_peers.sh
@@ -9,22 +9,49 @@ export REDISCLI_AUTH=$(echo $KEYDB_PASSWORD)
 # Extract local IPV6 address
 ip=$(grep fly-local-6pn /etc/hosts | awk '{print $1}')
 
-function detect_peers {
+detect_peers()
+{
   # Extract peer KeyDB server IPV6 addresses
   others=$( (dig aaaa global.$FLY_APP_NAME.internal @fdaa::3 +short | grep -v "$ip") || echo "")
-  # Add new peers using the CLI to avoid restarting Keydb
+  # Add new peers using the CLI to avoid restarting KeyDB
   if ( ps aux | grep -v grep | grep -q keydb-server ); then
     info=$(keydb-cli info replication)
     membercount=$(echo "$info" | grep "_host:" | wc -l)
-    echo "Replicas: $membercount"
+    echo "Detected replicas: $membercount"
     for i in $others; do
       if ! (echo $info | grep -q "_host:$i"); then # check masters
         echo "Adding peer $i with keydb-cli"
-        echo "replicaof $i 6379" | keydb-cli
+        keydb-cli replicaof $i 6379
       fi
     done
   fi
 }
+
+remove_from_peers()
+{
+  # Extract peer KeyDB server IPV6 addresses
+  others=$( (dig aaaa global.$FLY_APP_NAME.internal @fdaa::3 +short | grep -v "$ip") || echo "")
+  # Remove from peers using the CLI to avoid restarting KeyDB
+  if ( ps aux | grep -v grep | grep -q keydb-server ); then
+    info=$(keydb-cli info replication)
+    membercount=$(echo "$info" | grep "_host:" | wc -l)
+    echo "Detected replicas: $membercount"
+    for i in $others; do
+      if (echo $info | grep -q "_host:$i"); then # check masters
+        echo "Removing from peer $i with keydb-cli"
+        keydb-cli -h $i replicaof remove $ip 6379
+      fi
+    done
+  fi
+}
+
+shutdown() 
+{ 
+  echo "Shutdown signal!"
+  remove_from_peers
+}
+
+trap shutdown SIGTERM SIGINT
 
 # Check periodically
 while true; do
